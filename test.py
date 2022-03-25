55dc1e6c3794667f0cfebd8ba536a09700ebbe3d
pct <2319189860@qq.com>
2022-03-25 10:41:00 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+"""
+@File     : test.py
+@Project  : pycharm_git_temp
+@Time     : 2022/3/25 10:29
+@Author   : Pu Chengtao
+@Contact_2: 2319189860@qq.com
+@Software : PyCharm
+@Last Modify Time      @Version     @Desciption
+--------------------       --------        -----------
+2022/3/25 10:29        1.0             None
+"""
+
+print('hello,world')
